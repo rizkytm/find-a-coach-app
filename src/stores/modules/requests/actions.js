aaf529215a386ff1442c58b2b6d1efc1b1117e0f
rizkytm <ritrime97@gmail.com>
2020-12-27 11:25:02 +0700

    finised auth

--- src/stores/modules/requests/actions.js
+++ src/stores/modules/requests/actions.js
@@ -19,7 +19,8 @@ export default {
     },
     async fetchRequests(context) {
         const coachId = context.rootGetters.userId;
-        const response = await fetch(`https://find-a-coach-app-619ae-default-rtdb.firebaseio.com/requests/${coachId}.json`);
+        const token = context.rootGetters.token;
+        const response = await fetch(`https://find-a-coach-app-619ae-default-rtdb.firebaseio.com/requests/${coachId}.json?auth=` + token);
         const responseData = await response.json();
         if (!response.ok) {
             const error = new Error(responseData.message || 'Failed to fetch request.');
